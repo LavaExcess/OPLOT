4822ddd615bdda6aa0cabc532fa7158a5c229b71
LavaExcess <warz.702@yandex.ru>
2025-07-26 02:00:18 +0600

    adapting the header

--- src/components/ui/structure/Header.vue
+++ src/components/ui/structure/Header.vue
@@ -11,7 +11,7 @@
                     <NavigationMenuItem>
                         <NavigationMenuTrigger>{{ nav.contacts }}</NavigationMenuTrigger>
                         <NavigationMenuContent class="bg-[color:var(--background)]">
-                            <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                            <ul class="grid  gap-3 p-4 md:w-[600px] md:grid-cols-2 lg:w-[800px]">
                                 <li v-for="item in items" :key="item.urlId">
                                     <NavigationMenuLink as-child>
                                         <a :href="item.urlId"
@@ -34,15 +34,15 @@
                 </div>
                 <a href="#" class="header__link">Александровский Кирпич</a>
             </div>
-            <div class="header__nav">
-                <Button class="hero__button text-[color:var(--border2)] bg-[color:var(--background)]">
-                    <span class="hero__button-text">{{ nav.buttons.login }}</span>
+            <div class="header__nav hidden lg:flex">
+                <Button class="header__button text-[color:var(--border2)] bg-[color:var(--background)] ">
+                    <span class="header__button-text ">{{ nav.buttons.login }}</span>
                 </Button>
-                <Button class="hero__button bg-[color:var(--border2)]">
-                    <span class="hero__button-text">{{ nav.buttons.contact }}</span>
+                <Button class="header__button bg-[color:var(--border2)] ">
+                    <span class="header__button-text">{{ nav.buttons.contact }}</span>
                 </Button>
             </div>
-            <div class="header__burger md:hidden" @click.stop="toggleMobileMenu" role="button"
+            <div class="header__burger lg:hidden" @click.stop="toggleMobileMenu" role="button"
                 aria-label="Toggle mobile menu">
                 <div class="header__burger-line" />
                 <div class="header__burger-line" />
@@ -50,39 +50,40 @@
             </div>
         </div>
         <Transition name="slide-down">
-            <NavigationMenu v-if="isMobileMenuOpen" class="md:hidden">
+            <NavigationMenu v-if="isMobileMenuOpen" class="lg:hidden">
                 <div class="header__mobile-menu" @click.stop>
                     <NavigationMenuList class="flex flex-col">
-                        <NavigationMenuItem v-for="(item, index) in nav.menu" :key="index">
-                            <NavigationMenuLink :href="item.url">
-                                {{ item.label }}
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>{{ nav.contacts }}</NavigationMenuTrigger>
-                            <NavigationMenuContent class="bg-[color:var(--background)]">
-                                <ul class="grid gap-3 p-2 grid-cols-1 flex w-[330px]">
-                                    <li v-for="item in items" :key="item.urlId">
-                                        <NavigationMenuLink as-child>
-                                            <a :href="item.urlId"
-                                                class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                                <div class="text-sm font-medium leading-none">{{ item.title }}</div>
-                                                <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                                    {{ item.description }}
-                                                </p>
-                                            </a>
-                                        </NavigationMenuLink>
-                                    </li>
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-                        <div class="flex flex-col space-y-2 mt-4 px-2">
-                            <Button
-                                class="hero__button text-[color:var(--border2)] bg-[color:var(--background)] w-full">
-                                <span class="hero__button-text">{{ nav.buttons.login }}</span>
+                        <div class="md:hidden flex justify-center">
+                            <NavigationMenuItem v-for="(item, index) in nav.menu" :key="index">
+                                <NavigationMenuLink :href="item.url">
+                                    {{ item.label }}
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>{{ nav.contacts }}</NavigationMenuTrigger>
+                                <NavigationMenuContent class="bg-[color:var(--background)]">
+                                    <ul class="grid  gap-3 p-4 w-[300px]">
+                                        <li v-for="item in items" :key="item.urlId">
+                                            <NavigationMenuLink as-child>
+                                                <a :href="item.urlId"
+                                                    class="block select-none space-y-1 rounded-md p-3">
+                                                    <div class="text-sm font-medium leading-none">{{ item.title }}</div>
+                                                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                        {{ item.description }}
+                                                    </p>
+                                                </a>
+                                            </NavigationMenuLink>
+                                        </li>
+                                    </ul>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </div>
+                        <div class="header__nav lg:hidden flex justify-center">
+                            <Button class="header__button text-[color:var(--border2)] bg-[color:var(--background)] ">
+                                <span class="header__button-text ">{{ nav.buttons.login }}</span>
                             </Button>
-                            <Button class="hero__button bg-[color:var(--border2)] w-full">
-                                <span class="hero__button-text">{{ nav.buttons.contact }}</span>
+                            <Button class="header__button bg-[color:var(--border2)] ">
+                                <span class="header__button-text">{{ nav.buttons.contact }}</span>
                             </Button>
                         </div>
                     </NavigationMenuList>
@@ -145,7 +146,7 @@ onBeforeUnmount(() => {
 }
 
 .header__branding {
-    @apply flex items-center justify-center absolute left-1/2 transform -translate-x-1/2;
+    @apply flex items-center;
 }
 
 .header__logo {
@@ -153,23 +154,23 @@ onBeforeUnmount(() => {
 }
 
 .header__link {
-    @apply ml-2 font-bold text-lg md:text-xl lg:text-2xl text-[color:var(--foreground)];
+    @apply ml-2 font-bold text-lg lg:text-xl text-[color:var(--foreground)];
 }
 
 .header__nav {
-    @apply hidden md:flex items-center space-x-6;
+    @apply items-center space-x-2;
 }
 
-.hero__button {
-    @apply flex justify-center items-center px-3 py-2 w-[133px] md:w-[188px] h-10 md:h-12 border-2 border-[color:var(--border2)] cursor-pointer;
+.header__button {
+    @apply px-1 py-2 lg:w-[130px] md:w-full h-10 border-2 border-[color:var(--border2)] cursor-pointer;
 }
 
-.hero__button-text {
-    @apply px-2 md:px-4 font-medium text-sm tracking-[0.5px];
+.header__button-text {
+    @apply px-2 font-medium text-sm;
 }
 
 .header__burger {
-    @apply md:hidden flex flex-col space-y-1.5 w-6 cursor-pointer;
+    @apply lg:hidden flex flex-col space-y-1.5 w-6 cursor-pointer;
 }
 
 .header__burger-line {
@@ -177,6 +178,6 @@ onBeforeUnmount(() => {
 }
 
 .header__mobile-menu {
-    @apply fixed top-[72px] left-0 right-0 w-full shadow-md py-2 px-4 bg-[color:var(--background)] z-40;
+    @apply fixed top-[70px] left-0 right-0 w-full shadow-md py-2 px-4 bg-[color:var(--background)] z-40;
 }
 </style>
